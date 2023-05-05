45fd1a536f3064ff696ca86511b3c91a0866332f
Padyy1088 <crowpaddy@gmail.com>
2023-05-05 13:26:24 +0100

    new commit

--- src/components/graph/feedpercent.js
+++ src/components/graph/feedpercent.js
@@ -16,8 +16,7 @@ function FeedPercentGraph({pdata}) {
             <label className="label" style={{color: "grey"}}><b>FEED PERCENT</b></label>
             <br /><space style={{color: "#3A3B3B"}}>.</space>
             <Box>
-            <LineChart width={1125} height={343} data={pdata} margin={{ top: 10, right: 20, bottom: 1, left:-30}}>          
-            <CartesianGrid strokeDasharray="0.1 1" />
+            <LineChart width={1125} height={343} data={pdata} margin={{ top: 10, right: 20, bottom: 1, left:-30}}>   <CartesianGrid strokeDasharray="0.1 1" />       
             <XAxis dataKey="time"/>
             <YAxis dataKey="FeedPercent"/>
             <Tooltip/>
